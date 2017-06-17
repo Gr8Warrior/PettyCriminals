624c5509f703949ecf34b6ad9dd7cb40733919aa
shailendrasuriyal <shailendra.suriyal@intimetec.com>
2017-06-18 00:54:32 +0530

    Added implicit intent to send the report along with the suspect

--- app/src/main/java/com/pseudo/warriorz/pettycriminals/sqlite/CrimeBaseHelper.java
+++ app/src/main/java/com/pseudo/warriorz/pettycriminals/sqlite/CrimeBaseHelper.java
@@ -14,18 +14,20 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
 
     private static final int VERSION = 1;
     private static final String DATABASE_NAME = "crimeBase.db";
+
     public CrimeBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("Create table "+ CrimeTable.NAME +"("+
-                        "_id integer primary key autoincrement, "+
-                        CrimeTable.Cols.UUID +", "+
-                        CrimeTable.Cols.TITLE +", "+
-                        CrimeTable.Cols.DATE +", "+
-                        CrimeTable.Cols.SOLVED +
+        db.execSQL("Create table " + CrimeTable.NAME + "(" +
+                "_id integer primary key autoincrement, " +
+                CrimeTable.Cols.UUID + ", " +
+                CrimeTable.Cols.TITLE + ", " +
+                CrimeTable.Cols.DATE + ", " +
+                CrimeTable.Cols.SOLVED + ", " +
+                CrimeTable.Cols.SUSPECT +
                 ")"
         );
     }
